e1c892cecfa6b7be55449786e8b960c7f96b9261
bhargav2496 <bhargavvenkatesh2496@gmail.com>
2019-09-06 10:36:30 +0530

    1 file modified

--- components/Product.js
+++ components/Product.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const Product = ({id,name,description,image,price}) => {
+const Product = ({id,name,description,image,price,addFunc}) => {
   return(
 
     <article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10" id={id}>
@@ -10,7 +10,9 @@ const Product = ({id,name,description,image,price}) => {
     <h1 className="f3 mb2">{name}</h1>
     <h2 className="f5 fw4 gray mt0">{description}</h2>
 
-    <button className="f6 link dim br2 ba ph3 pv2 mb2 dib dark-pink">Add</button>
+    <button className="f6 link dim br2 ba ph3 pv2 mb2 dib dark-pink"
+    onClick={() => addFunc({id,name,description,image,price,units:1})}
+    >Add</button>
     <span>{price}</span>
   </div>
 </article>
